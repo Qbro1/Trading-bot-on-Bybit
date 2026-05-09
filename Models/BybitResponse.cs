73fbc6890e3ce9edc8e791f62ac430a1597f03a9
Qbro1 <kakashka5280@gmail.com>
2026-05-09 20:13:00 +0300

    Add files via upload

---
+++ Models/BybitResponse.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BybitApp1.Models
+{
+    public class BybitResponse
+    {
+
+        public string RetCode { get; set; }
+        public string RetMsg { get; set; }
+        public JObject Result { get; set; }
+
+    }
+}
