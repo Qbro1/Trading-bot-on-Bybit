d84c0c8844ddd6cc1726e86a9b856544b11875b3
Qbro1 <kakashka5280@gmail.com>
2026-05-09 20:17:53 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using BybitApp1.ViewModels;
+
+namespace BybitApp1
+{
+    public partial class MainPage : ContentPage
+    {
+      
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+       
+    }
+}
